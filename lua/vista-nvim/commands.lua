3845c21be40a4fb4a625e86bc4b0daf632871b18
u2087154 <yu.carl@outlook.com>
2023-02-01 21:11:38 +0000

    minor change

--- lua/vista-nvim/commands.lua
+++ lua/vista-nvim/commands.lua
@@ -1,24 +1,30 @@
 local command = {}
 
+local function tree_cmd()
+    if require("vista-nvim.view").is_win_open() then
+        require("vista-nvim.writer").structure_theme = "tree"
+        require("vista-nvim.handlers").update("lsp")
+    else
+        require("vista-nvim.writer").structure_theme = "tree"
+        require("vista-nvim").open()
+        require("vista-nvim.handlers").update("lsp")
+    end
+end
+
+local function type_cmd()
+    if require("vista-nvim.view").is_win_open() then
+        require("vista-nvim.writer").structure_theme = "type"
+        require("vista-nvim.handlers").update("lsp")
+    else
+        require("vista-nvim.writer").structure_theme = "type"
+        require("vista-nvim").open()
+        require("vista-nvim.handlers").update("lsp")
+    end
+end
+
 local subcommands = {
-    tree = function()
-        if require("vista-nvim.view").is_win_open() then
-            require("vista-nvim.writer").structure_theme = "tree"
-            require("vista-nvim.handlers").update("lsp")
-        else
-            require("vista-nvim.writer").structure_theme = "tree"
-            require("vista-nvim").open()
-        end
-    end,
-    type = function()
-        if require("vista-nvim.view").is_win_open() then
-            require("vista-nvim.writer").structure_theme = "type"
-            require("vista-nvim.handlers").update("lsp")
-        else
-            require("vista-nvim.writer").structure_theme = "type"
-            require("vista-nvim").open()
-        end
-    end,
+    tree = tree_cmd,
+    type = type_cmd,
 }
 
 function command.command_list()
